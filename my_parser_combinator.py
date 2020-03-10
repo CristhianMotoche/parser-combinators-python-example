737da84bd26703830b5aaaed6c3ad5d3fa74ac17
CristhianMotoche <cristhian.motoche@gmail.com>
2020-03-09 22:22:28 -0500

    Add parse method

--- my_parser_combinator.py
+++ my_parser_combinator.py
@@ -5,3 +5,20 @@ class Parser:
 
     def __call__(self, text):
         return self.fn(text)
+
+    def parse(self, text):
+        result = self(text)
+
+        if result:
+            return result
+        else:
+            raise ParseError(text)
+
+
+class ParseError(Exception):
+
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return "unexpected input: '%s'" % self.text
