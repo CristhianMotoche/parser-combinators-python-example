3e939eb0d30fd4b24606b3fe18ab0a2d584691ae
CristhianMotoche <cristhian.motoche@gmail.com>
2020-03-09 22:23:29 -0500

    String parser

--- my_parser_combinator.py
+++ my_parser_combinator.py
@@ -22,3 +22,13 @@ class ParseError(Exception):
 
     def __str__(self):
         return "unexpected input: '%s'" % self.text
+
+
+def string(s):
+    @Parser
+    def string_parser(text):
+        if text == s:
+            return s
+        else:
+            return None
+    return string_parser
