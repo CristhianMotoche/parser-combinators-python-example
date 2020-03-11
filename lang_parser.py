5dd9e03cbbce1b4f66e4eced2c54b1ca5fa1beb2
CristhianMotoche <cristhian.motoche@gmail.com>
2020-03-10 22:21:27 -0500

    Usage of map and desc

--- lang_parser.py
+++ lang_parser.py
@@ -1,6 +1,6 @@
 import parsy as p
 
-python_parser = p.string("Python")
+python_parser = p.string("Python").desc("The Awesome Python")
 haskell_parser = p.string("Haskell")
 java_parser = p.string("Java")
 css_parser = p.string("CSS")
@@ -10,4 +10,4 @@ lang_parser = (
     | haskell_parser
     | java_parser
     | css_parser
-)
+    ).map(lambda lang: lang.lower())
