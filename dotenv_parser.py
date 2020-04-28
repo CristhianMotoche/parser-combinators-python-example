04b0c1b1f96d8faa9ab466edd9e42abf5ab9b79b
CristhianMotoche <cristhian.motoche@gmail.com>
2020-04-27 21:34:45 -0500

    Parse many

--- dotenv_parser.py
+++ dotenv_parser.py
@@ -5,11 +5,12 @@ class EnvVar:
         self.key = key
         self.value = value
 
-
 key = p.regex(r"\w+")
 equal = p.string("=")
 value = p.regex(r"\w+")
 
 key_val = p.seq((key << equal), value).combine(EnvVar)
 
-dotenv_parser = key_val
+key_val_line = key_val << p.string("\n")
+
+dotenv_parser = key_val_line.many()
--- dotenv_parser_test.py
+++ dotenv_parser_test.py
@@ -4,5 +4,12 @@ def test_parser_returns_key_value():
     input_ = "USER=cmotoche"
     output = dotenv_parser.parse(input_)
 
-    assert output.key == "USER"
-    assert output.value == "cmotoche"
+    assert output[0].key == "USER"
+    assert output[0].value == "cmotoche"
+
+def test_parser_returns_lifst_of_items():
+    with open(".env") as f:
+        input_ = f.read()
+    output = dotenv_parser.parse(input_)
+
+    assert len(output) == 3
